eab64d3d703a676822c86cbe1d44a159c3894edf
mahdieh-ahmadi <m119.ahmadi@gmail.com>
2020-10-26 16:01:32 +0330

    Update k_means.m

--- k_means.m
+++ k_means.m
@@ -5,37 +5,30 @@ clc
 fid = dlmread('TrainData.txt');
 sizeData = size(fid);
 n = sizeData(1);
-k(1,:) = randi([0 1],[1 1200]);
-k(2,:) = randi([0 1],[1 1200]);
-k(3,:) = randi([0 1],[1 1200]);
-k(4,:) = randi([0 1],[1 1200]);
-k(5,:) = randi([0 1],[1 1200]);
-k(6,:) = randi([0 1],[1 1200]);
-k(7,:) = randi([0 1],[1 1200]);
-k(8,:) = randi([0 1],[1 1200]);
-k(9,:) = randi([0 1],[1 1200]);
-k(10,:) = randi([0 1],[1 1200]);
 
-% group(1)=[];
-% group(2)=[];
-% group(3)=[];
-% group(4)=[];
-% group(5)=[];
-% group(6)=[];
-% group(7)=[];
-% group(8)=[];
-% group(9)=[];
-% group(10)=[];
+knew = cell(1,10);
+k = cell(1,10);
 
-group = cell(1,10);
-
+k{1,1} = randi([0 1],[1 1200]);
+k{1,2} = randi([0 1],[1 1200]);
+k{1,3} = randi([0 1],[1 1200]);
+k{1,4} = randi([0 1],[1 1200]);
+k{1,5} = randi([0 1],[1 1200]);
+k{1,6} = randi([0 1],[1 1200]);
+k{1,7} = randi([0 1],[1 1200]);
+k{1,8} = randi([0 1],[1 1200]);
+k{1,9} = randi([0 1],[1 1200]);
+k{1,10} = randi([0 1],[1 1200]);
 
 space = zeros(1,10);
+Ji = inf;
 
-    for i=1:1:10
+while (Ji > 0.01)
+    group = cell(1,10);
+    for i=1:1:100
         newdata = fid(i,:);
         for j=1:1:10
-            space(1,j) = norm(k(j,:)-newdata);
+            space(1,j) = norm(k{1,j}-newdata);
         end
         [value , number ] = min(space);
         group{1,number} = [group{1,number};newdata];
@@ -44,13 +37,33 @@ space = zeros(1,10);
     %     imshow(maindata')
     end
     for i=1:10
-        if(size(group{1,i}) == 0)
-            knew(i,:) = k(i,:)
-        elseif(size(group{1,i}) == 1)
-            knew(i,:) = group{1,i}
+        s = size(group{1,i});
+        if(s(1) == 0)
+            knew{1,i} = k{1,i};
+        elseif(s(1) == 1)
+            knew{1,i} = group{1,i};
         else
-            knew(i,:) = mean(group{1,i})
+            knew{1,i} = mean(group{1,i});
         end
     end
-    % k1new = mean(group{1,7})
+    
+    Ji=0;
+    for i=1:1:10
+       s = size(group{1,i});
+       vectores = group{1,i};
+       sum = 0;
+       j = 1;
+       if(s(1) ~= 0)
+           for j=1:1:s(1)
+             sum = sum + norm( vectores(j,:)- knew{1,i})^2;
+           end
+       end
+       
+       sum = sum / j;
+       Ji = Ji+sum;
+    end
+    
+    k = knew;
+end
+    
 
